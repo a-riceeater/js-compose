369a2d63aa9095dd17f7554b09845f6aa7df81c7
a-riceeater <elijb12@gmail.com>
2024-11-18 22:00:55 -0800

    add min version to route

--- test/server.js
+++ test/server.js
@@ -9,6 +9,11 @@ app.get("/js-compose.js", (req, res) => {
     res.sendFile(path.join(__dirname, "../", "src", "js-compose.js"))
 })
 
+app.get("/js-compose.min.js", (req, res) => {
+    res.sendFile(path.join(__dirname, "../", "src", "js-compose.min.js"))
+})
+
+
 app.listen(80, () => {
     console.log("Server running on port :80")
 })
